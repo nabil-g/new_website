9624fd8ce99fd59803556e30d51a311d71b04d4e
nabil <nabil.ghedjati@gmail.com>
2017-09-09 16:15:11 +0200

    didn't committed since a long time

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,98 @@
 $(window).load(function () {
 
-  var term = $('.terminal');
-
-
-  var prompt = function () {
-    term.append("<br>~$  " + "<form class='prompt'><input id='promptField' type='text'></form>");
-    $('input').focus();
-
-    $('.prompt').submit(function (e) {
-      e.preventDefault();
-      console.log('submitted');
-      var input = $('#promptField').val();
-      if (input.length > 0) {
-        console.log('fffdf');
-        $(this).replaceWith($(this).children().val());
-        term.append('<span></span>');
-        $('.terminal:last-child').typed({
-          strings: ["Hi there. I'm a 25 y.o. junior web developer, full of ideas and motivation. To get more info, enter one of these commands."],
-          showCursor: false,
-          onStringTyped: prompt
-        });
-      }
+  $('main').fadeIn(1500, function () {
+    term.typed({
+      strings: [lang.greetings],
+      showCursor: false,
+      typeSpeed: 0,
+      onStringTyped: mainPromptCallback
     });
+  });
 
+  var term = $('.terminal');
+  var lang;
+  var msgData = {};
+
+// callback after different typed
+  var mainPromptCallback = function () {
+    $('#mainPromptField').val("");
+    $('#mainPrompt').show();
+    $('input:visible').focus();
+  };
+
+  var namePromptCallback = function () {
 
   };
 
-  term.typed({
-    strings: ["Hi there. I'm a 25 y.o. junior web developer, full of ideas and motivation. To get more info, enter one of these commands."],
-    showCursor: false,
-    onStringTyped: prompt
+  var mailPromptCallback = function () {
+
+  };
+
+  var msgPromptCallback = function () {
+
+  };
+
+
+// selecting the right language
+  if (localStorage.lang == "fr") {
+    // french version
+    lang = french;
+  } else if (localStorage.lang == "en") {
+    // english version
+    lang = english;
+  } else if (navigator.language == "fr" || "fr-BE" || "fr-CA"  || "fr-CH" || "fr-LU") {
+    // french version by default
+    lang = french;
+  } else {
+    // english version by default
+    lang = english;
+  }
+
+
+
+  $('#mainPrompt').submit(function (e) {
+    e.preventDefault();
+    var input = $("#mainPromptField").val();
+    input = input.toLowerCase();
+    if (input == "skills") {
+      $(this).hide();
+      term.append("~$  " + ($('#mainPromptField').val()) + "<br>");
+      term.append('<span></span>');
+      $('span:last-of-type').typed({
+        strings: ["x" + "<br>"],
+        showCursor: false,
+        onStringTyped: mainPromptCallback
+      });
+    }
   });
 
 
+  $('#namePrompt').submit(function (e) {
+    e.preventDefault();
+    var input = $("#namePromptField").val();
+    if (input.length > 0) {
+      $(this).hide();
+    }
+  });
 
+  $('#mailPrompt').submit(function (e) {
+    e.preventDefault();
+    var input = $("#mailPromptField").val();
+    if (input.length > 0) {
+      $(this).hide();
+    }
+  });
+
+  $('#msgPrompt').submit(function (e) {
+    e.preventDefault();
+    var input = $("#msgPromptField").val();
+    if (input.length > 0) {
+      $(this).hide();
+    }
+  });
+
+  $(document).click(function () {
+    $('input').focus();
+  });
 
 });
---
+++ src/js/translations.js
@@ -0,0 +1,7 @@
+var french = {
+  greetings : "<h1>Bonjour ! Je suis <a id='mainLink' href='#'>Nabil.Ghedjati</a>, un jeune développeur web, plein d'idées et de motivation.</h1><h3>Pour plus d'informations, veuillez entrer une de ces commandes :</h3> <ul><li>SKILLS</li><li>EXPERIENCE</li><li>HISTORY</li><li>PDF</li><li>ABOUT</li><li>LEGAL</li><li>CONTACT</li><li>HELP</li><li>LANG (FR/EN)</li></ul>"
+};
+
+var english = {
+  greetings: "Hi there ! I'm a 25 y.o. junior web developer, full of ideas and motivation. To get more info, enter one of these commands :  "
+};
